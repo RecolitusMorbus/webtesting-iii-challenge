cdc8bbe1d1fdc59331d0af8606a80b5831e5a29f
Nolan Allen <nolan.r.allen@gmail.com>
2019-06-19 21:35:42 -0500

    Initial commit: mistakenly cloned from Lambda, completely messed up the first attempt; cloned from forked repo and restarted project; first tests working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,34 @@
-// Test away!
+import React from 'react';
+import ReactDom from 'react-dom';
+import Controls from './Controls';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+
+describe('Lock button.', () => {
+  it('Should not function while gate is open.', () => {
+    const toggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls
+        toggleLocked={toggleLocked}
+        closed={false}
+        locked={false}
+      />
+    );
+    const button = getByText(/lock gate/i);
+    fireEvent.click(button);
+    expect(toggleLocked).toBeCalledTimes(0);
+  });
+  it('Changes text to \'Unlocked\' when gate is unlocked.', () => {
+    const toggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls
+        toggleLocked={toggleLocked}
+        closed={true}
+        locked={false}
+      />
+    );
+    const button = getByText(/lock gate/i);
+    fireEvent.click(button);
+    expect(toggleLocked).toBeCalledTimes(1);
+  });
+});
